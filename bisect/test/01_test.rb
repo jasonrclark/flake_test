551b206de6ddf960c35a23094a2580040fa8456d
Jason R. Clark <jclark@newrelic.com>
2014-06-15 22:39:34 -0700

    Updates for bisect example

--- bisect/test/01_test.rb
+++ bisect/test/01_test.rb
@@ -1,8 +1,7 @@
 require_relative 'test_helper'
 
-#require 'pry';require 'pry-nav'; binding.pry;
 class Test_01 < Test::Unit::TestCase
   def test_global
-    assert !$globals_are_awesome
+    refute $globals_are_awesome
   end
 end
--- bisect/test/05_test.rb
+++ bisect/test/05_test.rb
@@ -1,11 +1,8 @@
 require_relative 'test_helper'
 
 class Test_05 < Test::Unit::TestCase
-  def teardown
-    $globals_are_awesome = true
-  end
-
   def test_global
-    assert !$globals_are_awesome
+    $globals_are_awesome = true
+    assert $globals_are_awesome
   end
 end
